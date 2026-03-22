302552e089a3b693eef22cc6b5b290d227b9fac2
wasabina67 <wasabina67@gmail.com>
2026-03-22 13:52:45 +0900

    Update frontend/src/api/client.ts

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,4 +1,4 @@
 import createClient from 'openapi-fetch'
 import type { paths } from './schema.d.ts'
 
-export const api = createClient<paths>({ baseUrl: '/' })
+export const client = createClient<paths>({ baseUrl: '/' })
